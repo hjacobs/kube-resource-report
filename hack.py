a222c0387f39345eb128a990af4692bd54f77df3
Henning Jacobs <henning@jacobs1.de>
2018-02-11 13:01:56 +0100

    summary for multiple clusters

--- hack.py
+++ hack.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
 
 import collections
-import json
+import csv
 import logging
-import kubernetes.client
-import kubernetes.config
+import os
 import re
 import requests
+import cluster_discovery
 from urllib.parse import urljoin
-
-from requests.auth import AuthBase
+from pathlib import Path
 
 FACTORS = {
     'm': 1 / 1000,
@@ -35,26 +34,22 @@ def parse_resource(v):
     factor = FACTORS.get(match.group(2), 1)
     return int(match.group(1)) * factor
 
+NODE_COSTS_MONTHLY = {}
 
-class StaticAuthorizationHeaderAuth(AuthBase):
-    '''Static authentication with given "Authorization" header'''
-
-    def __init__(self, authorization):
-        self.authorization = authorization
-
-    def __call__(self, request):
-        request.headers['Authorization'] = self.authorization
-        return request
-
-
-class Cluster:
-    def __init__(self, id, api_server_url, ssl_ca_cert=None, auth=None, cert_file=None, key_file=None):
-        self.id = id
-        self.api_server_url = api_server_url
-        self.ssl_ca_cert = ssl_ca_cert
-        self.auth = auth
-        self.cert_file = cert_file
-        self.key_file = key_file
+# CSVs downloaded from https://ec2instances.info/
+for path in Path('.').glob('aws-ec2-costs-hourly-*.csv'):
+    region = path.stem.split('-', 4)[4]
+    with path.open() as fd:
+        reader = csv.DictReader(fd)
+        for row in reader:
+            cost = row['Linux On Demand cost']
+            if cost == 'unavailable':
+                continue
+            elif cost.startswith('$') and cost.endswith(' hourly'):
+                monthly_cost = float(cost.split()[0].strip('$')) * 24 * 30
+                NODE_COSTS_MONTHLY[(region, row['API Name'])] = monthly_cost
+            else:
+                raise Exception('Invalid')
 
 
 session = requests.Session()
@@ -69,48 +64,115 @@ def request(cluster, path, **kwargs):
     return session.get(urljoin(cluster.api_server_url, path), auth=cluster.auth, verify=cluster.ssl_ca_cert, **kwargs)
 
 
-logging.basicConfig()
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
-kubernetes.config.load_kube_config()
-config = kubernetes.client.configuration.Configuration()
-authorization = config.api_key.get('authorization')
-if authorization:
-    auth = StaticAuthorizationHeaderAuth(authorization)
-else:
-    auth = None
-cluster = Cluster(
-    'default',
-    config.host,
-    ssl_ca_cert=config.ssl_ca_cert,
-    cert_file=config.cert_file,
-    key_file=config.key_file,
-    auth=auth)
+def query_cluster(cluster):
+    pods = {}
+    nodes = {}
 
-pods = {}
+    response = request(cluster, '/api/v1/nodes')
+    response.raise_for_status()
+    cluster_capacity = collections.defaultdict(float)
+    cluster_allocatable = collections.defaultdict(float)
+    cluster_requests = collections.defaultdict(float)
+    cluster_usage = collections.defaultdict(float)
+    node_count = collections.defaultdict(int)
+    cluster_cost = 0
+    for node in response.json()['items']:
+        nodes[node['metadata']['name']] = node
+        for k, v in node['status'].get('capacity', {}).items():
+            cluster_capacity[k] += parse_resource(v)
+        for k, v in node['status'].get('allocatable', {}).items():
+            cluster_allocatable[k] += parse_resource(v)
+        role = node['metadata']['labels'].get('kubernetes.io/role')
+        node_count[role] += 1
+        region = node['metadata']['labels']['failure-domain.beta.kubernetes.io/region']
+        instance_type = node['metadata']['labels']['beta.kubernetes.io/instance-type']
+        node['role'] = role
+        node['instance_type'] = instance_type
+        node['cost'] = NODE_COSTS_MONTHLY.get((region, instance_type))
+        cluster_cost += node['cost']
 
-response = request(cluster, '/api/v1/pods')
-response.raise_for_status()
-for pod in response.json()['items']:
-    requests = collections.defaultdict(float)
-    for container in pod['spec']['containers']:
-        for k, v in container['resources'].get('requests', {}).items():
-            requests[k] += parse_resource(v)
-    pods[(pod['metadata']['namespace'], pod['metadata']['name'])] = {'requests': requests}
-
-response = request(cluster, '/api/v1/namespaces/kube-system/services/heapster/proxy/apis/metrics/v1alpha1/pods')
-response.raise_for_status()
-for item in response.json()['items']:
-    key = (item['metadata']['namespace'], item['metadata']['name'])
-    pod = pods.get(key)
-    if pod:
-        usage = collections.defaultdict(float)
-        for container in item['containers']:
-            for k, v in container.get('usage', {}).items():
+    response = request(cluster, '/api/v1/namespaces/kube-system/services/heapster/proxy/apis/metrics/v1alpha1/nodes')
+    response.raise_for_status()
+    for item in response.json()['items']:
+        key = item['metadata']['name']
+        node = nodes.get(key)
+        if node:
+            usage = collections.defaultdict(float)
+            for k, v in item.get('usage', {}).items():
                 usage[k] += parse_resource(v)
-        pod['usage'] = usage
+                cluster_usage[k] += parse_resource(v)
+            node['usage'] = usage
 
-for k, pod in sorted(pods.items()):
-    namespace, name = k
-    requests = pod['requests']
-    usage = pod.get('usage', collections.defaultdict(float))
-    print(namespace, '\t', name, '\t', requests['cpu'], '\t', requests['memory'], '\t', usage['cpu'], '\t', usage['memory'])
+
+    response = request(cluster, '/api/v1/pods')
+    response.raise_for_status()
+    for pod in response.json()['items']:
+        if pod['status'].get('phase') in ('Succeeded', 'Failed'):
+            # ignore completed pods
+            continue
+        requests = collections.defaultdict(float)
+        for container in pod['spec']['containers']:
+            for k, v in container['resources'].get('requests', {}).items():
+                requests[k] += parse_resource(v)
+                cluster_requests[k] += parse_resource(v)
+        pods[(pod['metadata']['namespace'], pod['metadata']['name'])] = {'requests': requests}
+
+    cluster_summary = {
+        'cluster': cluster,
+        'nodes': nodes,
+        'pods': pods,
+        'master_nodes': node_count['master'],
+        'worker_nodes': node_count['worker'],
+        'capacity': cluster_capacity,
+        'allocatable': cluster_allocatable,
+        'requests': cluster_requests,
+        'usage': cluster_usage,
+        'cost': cluster_cost
+    }
+
+    response = request(cluster, '/api/v1/namespaces/kube-system/services/heapster/proxy/apis/metrics/v1alpha1/pods')
+    response.raise_for_status()
+    for item in response.json()['items']:
+        key = (item['metadata']['namespace'], item['metadata']['name'])
+        pod = pods.get(key)
+        if pod:
+            usage = collections.defaultdict(float)
+            for container in item['containers']:
+                for k, v in container.get('usage', {}).items():
+                    usage[k] += parse_resource(v)
+            pod['usage'] = usage
+
+    for k, pod in sorted(pods.items()):
+        namespace, name = k
+        requests = pod['requests']
+        usage = pod.get('usage', collections.defaultdict(float))
+        print(namespace, '\t', name, '\t', requests['cpu'], '\t', requests['memory'], '\t', usage['cpu'], '\t', usage['memory'])
+
+    return cluster_summary
+
+cluster_summaries = {}
+discoverer = cluster_discovery.KubeconfigDiscoverer(Path(os.path.expanduser('~/.kube/config')), set())
+for cluster in discoverer.get_clusters():
+    try:
+        logger.debug('Querying cluster {} ({})..'.format(cluster.id, cluster.api_server_url))
+        summary = query_cluster(cluster)
+        cluster_summaries[cluster.id] = summary
+    except Exception as e:
+        print(e)
+
+for cluster_id, summary in sorted(cluster_summaries.items()):
+    worker_instance_type = set()
+    for node in summary['nodes'].values():
+        if node['role'] == 'worker':
+            worker_instance_type.add(node['instance_type'])
+    fields = [cluster_id, summary['master_nodes'], summary['worker_nodes'], ','.join(worker_instance_type)]
+    for x in ['capacity', 'allocatable', 'requests', 'usage']:
+        fields += [round(summary[x]['cpu'], 2), int(summary[x]['memory'] / (1024*1024))]
+    fields += [round(summary['cost'], 2)]
+    for f in fields:
+        print(f, '\t', end='')
+    print()
+
