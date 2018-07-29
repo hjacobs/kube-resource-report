32277e5893bb64c551b60d32407ed97e4ffdc962
Henning Jacobs <henning@zalando.de>
2018-07-29 21:24:21 +0200

    assets folder

--- kube_resource_report/report.py
+++ kube_resource_report/report.py
@@ -653,8 +653,11 @@ def generate_report(
         context["team"] = team
         template.stream(**context).dump(str(output_path / file_name))
 
+    assets_path = output_path / "assets"
+    assets_path.mkdir(exist_ok=True)
+
     for path in templates_path.iterdir():
         if path.match("*.js") or path.match("*.css") or path.match("*.png"):
-            shutil.copy(str(path), str(output_path / path.name))
+            shutil.copy(str(path), str(assets_path / path.name))
 
     return cluster_summaries
