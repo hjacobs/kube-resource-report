cb245bae2fe5c929e50af9f81187bb578c005f7c
Tomasz Święcicki <tswiecicki@vewd.com>
2019-07-29 11:20:19 +0200

    fix tests

--- tests/test_report.py
+++ tests/test_report.py
@@ -175,7 +175,9 @@ def fake_generate_report(output_dir, monkeypatch):
             set(["kube-system"]),
             None,
             None,
-            100.0,
+            100.0,  # additional_cost_per_cluster
+            1.0,  # alpha ema
+            {},
             None,
             None,
             ["worker", "node"]
@@ -240,7 +242,7 @@ def test_get_pod_usage(monkeypatch, fake_pod_metric_responses):
     mock_client = get_mock_client(fake_pod_metric_responses)
     cluster = Cluster("test-cluster-1", "test-cluster-1", "https://test-cluster-1.example.org", mock_client)
     pods = {('default', 'pod-1'): {'usage': new_resources()}}
-    get_pod_usage(cluster, pods)
+    get_pod_usage(cluster, pods, {}, 1.0)
     assert pods[('default', 'pod-1')]['usage']['cpu'] == 0.05
 
 
@@ -248,7 +250,7 @@ def test_get_node_usage(monkeypatch, fake_node_metric_responses):
     mock_client = get_mock_client(fake_node_metric_responses)
     cluster = Cluster("test-cluster-1", "test-cluster-1", "https://test-cluster-1.example.org", mock_client)
     nodes = {'node-1': {'usage': new_resources()}}
-    get_node_usage(cluster, nodes)
+    get_node_usage(cluster, nodes, {}, 1.0)
     assert nodes['node-1']['usage']['cpu'] == 16
 
 
